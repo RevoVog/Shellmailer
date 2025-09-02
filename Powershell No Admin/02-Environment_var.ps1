387db4316f86883b11022cdbd1279c3ebef72af3
RevoVog <vihanga.vog@gmail.com>
2025-09-02 23:19:38 +0530

    List of environment variables available to the current user session command PS

---
+++ Powershell No Admin/02-Environment_var.ps1
@@ -0,0 +1,54 @@
+Get-ChildItem Env:
+
+
+# Lists all environment variables available to the current user session.
+# No need to run as administrator
+
+# Name                           Value
+# ----                           -----
+# ALLUSERSPROFILE                xxxxxxxx
+# APPDATA                        xxxxxxxx
+# ChocolateyInstall              xxxxxxxx
+# ChocolateyLastPathUpdate       xxxxxxxx
+# CommonProgramFiles             xxxxxxxx
+# CommonProgramFiles(x86)        xxxxxxxx
+# CommonProgramW6432             xxxxxxxx
+# COMPUTERNAME                   xxxxxxxx
+# ComSpec                        xxxxxxxx
+# configsetroot                  xxxxxxxx
+# DriverData                     xxxxxxxx
+# EFC_16108_1592913036           xxxxxxxx
+# HOMEDRIVE                      xxxxxxxx
+# HOMEPATH                       xxxxxxxx
+# LOCALAPPDATA                   xxxxxxxx
+# LOGONSERVER                    xxxxxxxx
+# NUMBER_OF_PROCESSORS           xxxxxxxx
+# OneDrive                       xxxxxxxx
+# OS                             xxxxxxxx
+# Path                           xxxxxxxx
+# PATHEXT                        xxxxxxxx
+# PROCESSOR_ARCHITECTURE         xxxxxxxx
+# PROCESSOR_IDENTIFIER           xxxxxxxx
+# PROCESSOR_LEVEL                xxxxxxxx
+# PROCESSOR_REVISION             xxxxxxxx
+# ProgramData                    xxxxxxxx
+# ProgramFiles                   xxxxxxxx
+# ProgramFiles(x86)              xxxxxxxx
+# ProgramW6432                   xxxxxxxx
+# PSModulePath                   xxxxxxxx
+# PT8HOME                        xxxxxxxx
+# PUBLIC                         xxxxxxxx
+# SESSIONNAME                    xxxxxxxx
+# SystemDrive                    xxxxxxxx
+# SystemRoot                     xxxxxxxx
+# TEMP                           xxxxxxxx
+# TMP                            xxxxxxxx
+# USERDOMAIN                     xxxxxxxx
+# USERDOMAIN_ROAMINGPROFILE      xxxxxxxx
+# USERNAME                       xxxxxxxx
+# USERPROFILE                    xxxxxxxx
+# VBOX_MSI_INSTALL_PATH          xxxxxxxx
+# windir                         xxxxxxxx
+# ZES_ENABLE_SYSMAN              xxxxxxxx|
+# And more....
+
