250c7a79f0ad8c691aba0ae872c62b3445bb7fa5
RevoVog <vihanga.vog@gmail.com>
2025-09-02 23:25:02 +0530

    Network Configs Command PS

---
+++ Powershell No Admin/03-Network_configs.ps1
@@ -0,0 +1,16 @@
+Get-NetIPConfiguration
+
+# Network configuration, including IP address, DNS servers, and adapter information.
+# No need to run as administrator
+
+# InterfaceAlias       : xxxxxxxx
+# InterfaceIndex       : xxxxxxxx
+# InterfaceDescription : xxxxxxxx
+# NetProfile.Name      : xxxxxxxx
+# IPv6Address          : xxxxxxxx
+# IPv4Address          : xxxxxxxx
+# IPv6DefaultGateway   : xxxxxxx
+# IPv4DefaultGateway   : xxxxxxx
+# DNSServer            : xxxxxxx
+
+# And more...
