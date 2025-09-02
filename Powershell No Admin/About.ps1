26742d3414369e1440f3de69e0b3def417100fa8
RevoVog <vihanga.vog@gmail.com>
2025-09-02 23:11:24 +0530

    About user info command PS

---
+++ Powershell No Admin/About.ps1
@@ -0,0 +1,11 @@
+Get-WmiObject -Class Win32_ComputerSystem
+
+#Retrieve details about the computer’s make, model, and user information.
+#Don't need to run as administrator
+
+# Domain              : xxxxxxxx
+# Manufacturer        : xxxxxxxx
+# Model               : xxxxxxxx
+# Name                : xxxxxxxx
+# PrimaryOwnerName    : xxxxxxxx
+# TotalPhysicalMemory : xxxxxxxx
