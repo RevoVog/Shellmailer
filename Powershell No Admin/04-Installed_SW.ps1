a232ce05d3f1bd0b515201d7fdfda59748d738ac
RevoVog <vihanga.vog@gmail.com>
2025-09-02 23:29:16 +0530

    Installed Software listing cammand PS

---
+++ Powershell No Admin/04-Installed_SW.ps1
@@ -0,0 +1,7 @@
+Get-ChildItem -Path "$Env:UserProfile\AppData\Local\Programs"
+
+# Lists all installed software for the current user by checking the local AppData Programs directory.
+# No need to run as administrator
+
+# Mode                 LastWriteTime         Length Name
+# ----                 -------------         ------ ----
